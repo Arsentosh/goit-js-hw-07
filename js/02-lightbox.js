daec1c8791759dced20bd740e33ef8addd74d9f8
Arsentosh <doncenkoarsenij185@gmail.com>
2023-09-02 10:31:26 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -23,34 +23,10 @@ function createGallery(arr) {
 }
 
 function handlePhotoClick(event) {
-  if (event.target === event.currentTarget) {
-    return;
-  }
-
   event.preventDefault();
-
-  // const modal = lightbox.create(`<img
-  //       class="gallery__image"
-  //       src="${event.target.dataset.source}"
-  //       alt="${event.target.alt}"
-
-  //       />`);
-
-  // const lightbox =
-
-  new SimpleLightbox(".gallery a", {
-    sourceAttr: "href",
-    captionsData: "alt",
-    captionDelay: 10,
-  });
-  // lightbox.show();
-  console.log(lightbox);
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      modal.close();
-    }
-  });
 }
 
-console.log(galleryItems);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
